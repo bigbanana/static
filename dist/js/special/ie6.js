4496b4d76afa71ee00f9671fe1a4e266ad683912
zhangpeng <skxtay@gmail.com>
2015-03-27 18:22:46 +0800

    z

--- dist/js/special/ie6.js
+++ dist/js/special/ie6.js
@@ -1,2 +1,2 @@
-/* build : 564493634@qq.com 2015-03-27 18:10:13 */
+/* build : 564493634@qq.com 2015-03-27 18:21:33 */
 define("ie6",["jquery","DD_belatedPNG"],function(e,n){e(function(){n.fix("img,.png");try{document.execCommand("BackgroundImageCache",!1,!0)}catch(e){}})});
