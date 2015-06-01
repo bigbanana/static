955049826efb4c9b89e88ac4c7407318d1df78fe
zhangpeng <skxtay@gmail.com>
2015-06-01 16:00:58 +0800

    zzz

--- demos/html5shivRespond/js/index.js
+++ demos/html5shivRespond/js/index.js
@@ -1,4 +1,4 @@
-require(['jquery','modernizr','respond'],function($,modernizr){
+require(['jquery','modernizr'],function($,modernizr){
   $(window).on('load',function(){
     $("#html5shiv span").text($('html').attr('class'));
   });
