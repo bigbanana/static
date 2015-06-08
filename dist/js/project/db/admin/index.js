e08513c961ac4a51c81dd617b82f305a79947f20
Your Name <you@example.com>
2015-06-08 13:40:01 +0800

    delete dist dir

--- dist/js/project/db/admin/index.js
+++
@@ -1,2 +0,0 @@
-/* build : 564493634@qq.com 2015-06-03 15:45:35 */
-require(["jquery","cookie","underscore","jquery.ui"],function(e,n,t){e(function(){var t=(e(document.body),e(".page-nav")),a=e(".page-tab"),o=e(".theme");t.accordion(),a.tabs().find(".ui-tabs-nav").sortable({axis:"x",stop:function(){a.tabs("refresh")}}),o.selectmenu({change:function(){n.set("theme",e(this).val(),{expires:365}),location.reload()}})})});
--- dist/js/project/db/index.js
+++
@@ -1,2 +0,0 @@
-/* build : 564493634@qq.com 2015-06-03 15:45:35 */
-require(["jquery"],function(){});
