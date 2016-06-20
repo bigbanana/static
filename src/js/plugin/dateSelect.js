c3703f21b02ae3944828c9b438c4a2b2af4956f1
zhangpeng <skxtay@gmail.com>
2016-06-20 17:09:07 +0800

    zz

--- src/js/plugin/dateSelect.js
+++ src/js/plugin/dateSelect.js
@@ -11,6 +11,7 @@
     factory( jQuery,_,moment,widget );
   }
 }(function($,_,moment,widget){
+  window.moment = moment;
   var DateSelect = function(opt){
     this.options = $.extend(true,{},arguments.callee.options,opt);
     this.$el = $(opt.el);
@@ -19,26 +20,24 @@
 
   $.extend(DateSelect.prototype,{
     init: function(){
-      this.date = moment();
+      this.date = moment(this.$el.val()||undefined);
       this.$el.wrap('<div class="ui-date-select"></div>').hide();
       this.$wrap = this.$el.parent();
       this.$year = $('<select></select>').width(this.options.widths[0]).appendTo(this.$wrap);
       this.$month = $('<select></select>').width(this.options.widths[1]);
       this.options.hasmonth && this.$month.appendTo(this.$wrap);
-      this.$date = $('<select></select>').width(this.options.widths[0]);
+      this.$date = $('<select></select>').width(this.options.widths[2]);
       this.options.hasmonth && this.options.hasdate && this.$date.appendTo(this.$wrap);
-      this.updateYear();
-      this.updateMonth();
-      this.updateDate();
-      this.setValue();
       this.event();
+      this.updateYear();
+      this.$year.trigger('change');
     },
     event: function(){
       var that = this;
       this.$year.on('change',function(){
         that.date.set('year',parseInt(this.value));
-        that.updateMonth();
         that.updateYear();
+        that.updateMonth();
         that.setValue();
       });
       this.$month.on('change',function(){
@@ -52,13 +51,18 @@
       });
     },
     updateYear: function(){
-      var opts=[],year = this.date.get('year'),that = this;
+      var opts=[],year = this.date.get('year'),that = this,cur;
       this.$year.dropdownSelect('destory');
       this.$year.empty();
-      opts.push(year);
+      cur = year;
+      if(isNaN(year)){
+        cur = new Date().getFullYear();
+      }
+      this.options.invalide && that.$year.append('<option value="">请选择</option>');
+      opts.push(cur);
       for(var i=1;i<this.options.range+1;i++){
-        opts.unshift(year-i);
-        opts.push(year+i);
+        opts.unshift(cur-i);
+        opts.push(cur+i);
       }
       _.each(opts,function(item){
         that.$year.append('<option value="'+item+'"'+(item==year&&' selected')+'>'+item+'</option>');
@@ -74,6 +78,8 @@
         that.$month.append('<option value="'+i+'"'+(i==month&&' selected')+'>'+(i+1)+'</option>');
       }
       this.$month.dropdownSelect({className:'ml5'});
+      //时间不合法就隐藏
+      this.$month.data('dropdownSelect').$widget.toggle(!isNaN(this.date.unix()));
     },
     updateDate: function(){
       var opts=[],
@@ -89,7 +95,12 @@
       this.$date.data('dropdownSelect').$selectMenu.width(60);
     },
     setValue: function(){
-      this.$el.val(this.options.format && this.date.format(this.options.format) || this.date.unix());
+      var unix = this.date.unix();
+      if(isNaN(unix)){
+        this.$el.val('');
+      }else{
+        this.$el.val(this.date.format(this.options.format));
+      }
       this.$el.trigger('change');
     }
   });
@@ -99,8 +110,9 @@
       hasmonth: true,
       hasdate: true,
       widths:[78,50,50],
+      invalide: true,//允许选择空时间
       range: 20, //年份跨度（前后20年）
-      format: '' //YYYY-MM-DD
+      format: 'YYYY-MM-DD' //YYYY-MM-DD
     }
   });
 
