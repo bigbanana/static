c7778a6a234d89f2650a8b693009b90aecaa55db
zhangpeng <skxtay@gmail.com>
2016-09-01 15:40:17 +0800

    zzz

--- src/js/plugin/jquery.widget.js
+++ src/js/plugin/jquery.widget.js
@@ -59,6 +59,7 @@
 
         widgetDefs = $.map(widgets,function(item){
           var widgetDef = $.Deferred();
+          console.log(item);
           require(['jquery.'+item],function(){
             $el[item] && $el[item](data);
             widgetDef.resolve();
